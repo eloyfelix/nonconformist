f54e2070aa0a1bfbd25696ca6407e1d70e62c35d
Eloy Felix <eloyfelix@gmail.com>
2020-05-27 21:41:54 +0100

    add test for nc_factory

--- test/tests.py
+++ test/tests.py
@@ -21,8 +21,10 @@ from nonconformist.icp import IcpClassifier, IcpRegressor
 from nonconformist.icp import OobCpClassifier, OobCpRegressor
 
 from nonconformist.nc import MarginErrFunc
+from nonconformist.nc import NcFactory
 from nonconformist.nc import ClassifierNc, RegressorNc, RegressorNormalizer
 from nonconformist.nc import AbsErrorErrFunc, SignErrorErrFunc
+
 from nonconformist.cp import TcpClassifier
 
 from nonconformist.evaluation import cross_val_score
@@ -469,3 +471,146 @@ class TestNonconformist(unittest.TestCase):
         table = np.vstack([prediction.T, data.target[test]]).T
         df = pd.DataFrame(np.vstack([header, table]))
         print(df)
+
+
+    def test_nc_factory(self):
+
+        def score_model(icp, icp_name, ds, ds_name, scoring_funcs):
+            scores = cross_val_score(
+                icp,
+                ds.data,
+                ds.target,
+                iterations=10,
+                folds=10,
+                scoring_funcs=scoring_funcs,
+                significance_levels=[0.05, 0.1, 0.2],
+            )
+
+            print("\n{}: {}".format(icp_name, ds_name))
+            scores = scores.drop(["fold", "iter"], axis=1)
+            print(scores.groupby(["significance"]).mean())
+
+        # -----------------------------------------------------------------------------
+        # Classification
+        # -----------------------------------------------------------------------------
+        data = load_iris()
+
+        nc = NcFactory.create_nc(RandomForestClassifier(n_estimators=100))
+        icp = IcpClassifier(nc)
+        icp_cv = ClassIcpCvHelper(icp)
+        score_model(icp_cv, "IcpClassifier", data, "iris", [class_mean_errors, class_avg_c])
+
+        # -----------------------------------------------------------------------------
+        # Classification (normalized)
+        # -----------------------------------------------------------------------------
+        data = load_iris()
+
+        nc = NcFactory.create_nc(
+            RandomForestClassifier(n_estimators=100), normalizer_model=KNeighborsRegressor()
+        )
+        icp = IcpClassifier(nc)
+        icp_cv = ClassIcpCvHelper(icp)
+
+        score_model(
+            icp_cv, "IcpClassifier (normalized)", data, "iris", [class_mean_errors, class_avg_c]
+        )
+
+        # -----------------------------------------------------------------------------
+        # Classification OOB
+        # -----------------------------------------------------------------------------
+        data = load_iris()
+
+        nc = NcFactory.create_nc(
+            RandomForestClassifier(n_estimators=100, oob_score=True), oob=True
+        )
+        icp_cv = OobCpClassifier(nc)
+
+        score_model(
+            icp_cv, "IcpClassifier (OOB)", data, "iris", [class_mean_errors, class_avg_c]
+        )
+
+        # -----------------------------------------------------------------------------
+        # Classification OOB normalized
+        # -----------------------------------------------------------------------------
+        data = load_iris()
+
+        nc = NcFactory.create_nc(
+            RandomForestClassifier(n_estimators=100, oob_score=True),
+            oob=True,
+            normalizer_model=KNeighborsRegressor(),
+        )
+        icp_cv = OobCpClassifier(nc)
+
+        score_model(
+            icp_cv,
+            "IcpClassifier (OOB, normalized)",
+            data,
+            "iris",
+            [class_mean_errors, class_avg_c],
+        )
+
+        # -----------------------------------------------------------------------------
+        # Regression
+        # -----------------------------------------------------------------------------
+        data = load_diabetes()
+
+        nc = NcFactory.create_nc(RandomForestRegressor(n_estimators=100))
+        icp = IcpRegressor(nc)
+        icp_cv = RegIcpCvHelper(icp)
+
+        score_model(
+            icp_cv, "IcpRegressor", data, "diabetes", [reg_mean_errors, reg_median_size]
+        )
+
+        # -----------------------------------------------------------------------------
+        # Regression (normalized)
+        # -----------------------------------------------------------------------------
+        data = load_diabetes()
+
+        nc = NcFactory.create_nc(
+            RandomForestRegressor(n_estimators=100), normalizer_model=KNeighborsRegressor()
+        )
+        icp = IcpRegressor(nc)
+        icp_cv = RegIcpCvHelper(icp)
+
+        score_model(
+            icp_cv,
+            "IcpRegressor (normalized)",
+            data,
+            "diabetes",
+            [reg_mean_errors, reg_median_size],
+        )
+
+        # -----------------------------------------------------------------------------
+        # Regression OOB
+        # -----------------------------------------------------------------------------
+        data = load_diabetes()
+
+        nc = NcFactory.create_nc(
+            RandomForestRegressor(n_estimators=100, oob_score=True), oob=True
+        )
+        icp_cv = OobCpRegressor(nc)
+
+        score_model(
+            icp_cv, "IcpRegressor (OOB)", data, "diabetes", [reg_mean_errors, reg_median_size]
+        )
+
+        # -----------------------------------------------------------------------------
+        # Regression OOB normalized
+        # -----------------------------------------------------------------------------
+        data = load_diabetes()
+
+        nc = NcFactory.create_nc(
+            RandomForestRegressor(n_estimators=100, oob_score=True),
+            oob=True,
+            normalizer_model=KNeighborsRegressor(),
+        )
+        icp_cv = OobCpRegressor(nc)
+
+        score_model(
+            icp_cv,
+            "IcpRegressor (OOB, normalized)",
+            data,
+            "diabetes",
+            [reg_mean_errors, reg_median_size],
+        )
